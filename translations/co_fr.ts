58a0dd79a67f29808371a15d8898bbcb0e8ad6d9
Pierre Amadio <pierre.amadio@libertysurf.fr>
2011-09-23 21:58:39 +0200

    translating stuff

--- translations/co_fr.ts
+++ translations/co_fr.ts
@@ -100,6 +100,11 @@
         <source>CHA</source>
         <translation type="unfinished">CHA</translation>
     </message>
+    <message>
+        <location filename="createcharacterform.cpp" line="95"/>
+        <source>Character Creation Form.</source>
+        <translation type="unfinished">Formulaire de création de personnage.</translation>
+    </message>
 </context>
 <context>
     <name>DiceView</name>
@@ -114,12 +119,12 @@
         <translation type="unfinished">TextLabel</translation>
     </message>
     <message>
-        <location filename="diceview.cpp" line="48"/>
+        <location filename="diceview.cpp" line="66"/>
         <source>Image Viewer</source>
         <translation type="unfinished">ImageViewer</translation>
     </message>
     <message>
-        <location filename="diceview.cpp" line="49"/>
+        <location filename="diceview.cpp" line="67"/>
         <source>Cannot load %1.</source>
         <translation type="unfinished">Impossible de charger %1.</translation>
     </message>
@@ -147,42 +152,68 @@
         <translation type="unfinished">Fichier</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="84"/>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>Help</source>
+        <translation type="unfinished">Aide</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
         <source>Quit</source>
         <translation type="unfinished">Quitter</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="89"/>
+        <location filename="mainwindow.ui" line="96"/>
         <source>New Character</source>
         <translation type="unfinished">Nouveau personnage</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="94"/>
+        <location filename="mainwindow.ui" line="101"/>
         <source>New Character (free input)</source>
         <translation type="unfinished">Nouveau personnage (sans jets)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="132"/>
+        <location filename="mainwindow.ui" line="106"/>
+        <source>About this application</source>
+        <oldsource>About this applictation</oldsource>
+        <translation type="unfinished">A propos de cette application</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="59"/>
+        <source>Forgotten Chronicles</source>
+        <translation type="unfinished">Chroniques Oubliées</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="76"/>
+        <source>About CO</source>
+        <translation type="unfinished">A propos de CO</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="77"/>
+        <source>&lt;h2&gt;Forgotten Chronicles Character Sheet Generator&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2011 Pierre Amadio &amp;lt;pierre.amadio@libertysurf.fr&amp;gt;&lt;p&gt;This application is released under the GPL Licence. This mean you have the right to run it, access its source code, modify it, and redistribute it (as long as you give the same right to whoever you redistribute the original, or a modified version).&lt;p&gt;More details are available on http://www.gnu.org/licenses/&lt;p&gt;The source code is available on https://gitorious.org/co-character-generator </source>
+        <translation type="unfinished">&lt;h&gt;Création de personnage pour Chroniques Oubliées&lt;/h2&gt;&lt;p&gt;Copyright &amp;copy; 2011 Pierre Amadio &amp;lt;pierre.amadio@libertysurf.fr&amp;gt&lt;p&gt;Cette application est distribuée sous la licence GPL. Ceci veux dire que vous avez le droit de l&apos;utiliser, de lire son code source, de le modifier, et de redistribuer l&apos;original ou une version modifiée (dans la mesure ou vous donnez les mêmes droit à la personne à qui vous distribuez le logiciel). &lt;p&gt; Pour plus d&apos;information, voyez http://www.gnu.org/licences/.&lt;p&gt;Le code source est disponible à https://gitorious.org/co-character-generator</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="172"/>
         <source>Are you sure you want to delete %1 ?</source>
         <translation type="unfinished">Etes vous vraiment sur de vouloir effacer %1 ?</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="136"/>
+        <location filename="mainwindow.cpp" line="176"/>
         <source>Deleting a user</source>
         <translation type="unfinished">Elimination d&apos;un personnage</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="244"/>
+        <location filename="mainwindow.cpp" line="286"/>
         <source>Name</source>
         <translation type="unfinished">Nom</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="245"/>
+        <location filename="mainwindow.cpp" line="287"/>
         <source>Class</source>
         <translation type="unfinished">Classe</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="246"/>
+        <location filename="mainwindow.cpp" line="288"/>
         <source>Race</source>
         <translation type="unfinished">Race</translation>
     </message>
@@ -190,62 +221,62 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="corules.cpp" line="44"/>
+        <location filename="corules.cpp" line="61"/>
         <source>Dwarf</source>
         <translation type="unfinished">Nain</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="45"/>
+        <location filename="corules.cpp" line="62"/>
         <source>Elf</source>
         <translation type="unfinished">Elfe</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="46"/>
+        <location filename="corules.cpp" line="63"/>
         <source>Half Elf</source>
         <translation type="unfinished">Demi Elfe</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="47"/>
+        <location filename="corules.cpp" line="64"/>
         <source>Human</source>
         <translation type="unfinished">Humain</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="48"/>
+        <location filename="corules.cpp" line="65"/>
         <source>Orc</source>
         <translation type="unfinished">Orc</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="51"/>
+        <location filename="corules.cpp" line="68"/>
         <source>Alien</source>
         <translation type="unfinished">Extra terrestre</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="56"/>
+        <location filename="corules.cpp" line="73"/>
         <source>Warrior</source>
         <translation type="unfinished">Guerrier</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="57"/>
+        <location filename="corules.cpp" line="74"/>
         <source>Wizard</source>
         <translation type="unfinished">Magicien</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="58"/>
+        <location filename="corules.cpp" line="75"/>
         <source>Priest</source>
         <translation type="unfinished">Prêtre</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="59"/>
+        <location filename="corules.cpp" line="76"/>
         <source>Ranger</source>
         <translation type="unfinished">Rodeur</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="60"/>
+        <location filename="corules.cpp" line="77"/>
         <source>Thief</source>
         <translation type="unfinished">Voleur</translation>
     </message>
     <message>
-        <location filename="corules.cpp" line="62"/>
+        <location filename="corules.cpp" line="79"/>
         <source>None</source>
         <translation type="unfinished">Aucune</translation>
     </message>
@@ -263,7 +294,7 @@
     <message>
         <location filename="charactersheetviewerform.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished">Dialog</translation>
+        <translation type="unfinished">Dialogue</translation>
     </message>
     <message>
         <location filename="charactersheetviewerform.ui" line="39"/>
@@ -274,6 +305,11 @@
         <location filename="charactersheetviewerform.ui" line="59"/>
         <source>Close</source>
         <translation type="unfinished">Fermer</translation>
+    </message>
+    <message>
+        <location filename="charactersheetviewerform.cpp" line="38"/>
+        <source>Character Sheet.</source>
+        <translation type="unfinished">Feuille de personnage</translation>
     </message>
 </context>
 <context>
@@ -295,6 +331,11 @@
         <location filename="randomcaracform.ui" line="47"/>
         <source>&amp;Roll</source>
         <translation type="unfinished">&amp;Jet</translation>
+    </message>
+    <message>
+        <location filename="randomcaracform.cpp" line="38"/>
+        <source>Dice Roll Emulator.</source>
+        <translation type="unfinished">Simualteur de dés.</translation>
     </message>
 </context>
 </TS>
